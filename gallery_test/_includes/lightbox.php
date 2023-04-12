d2b1a3d6603fa38b08e065d00515348cc4dbe6ac
lilbud <bvw20002010@gmail.com>
2023-04-12 15:17:24 -0400

    testing

---
+++ gallery_test/_includes/lightbox.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<script src="js/lightbox.min.js"></script>
+<script src="js/lightbox-plus-jquery.min.js"></script>
+
+<script>
+    lightbox.option({
+        'resizeDuration': 0,
+        'wrapAround': true,
+        'fitImagesInViewport': true,
+        'imageFadeDuration': 0,
+        'alwaysShowNavOnTouchDevices': true
+    })
+</script>
